07ae8e06013b5081b88f16045d952741960809e1
kimi <kimi>
2026-08-29 20:15:51 +0800

    test(netns): model restricted cone mapping

--- tests/netns/topology.sh
+++ tests/netns/topology.sh
@@ -225,8 +225,10 @@ EOF
         else
             ip netns exec "$namespace" nft add rule ip rustgo_netns postrouting oifname ext0 snat to "$external_ip"
         fi
-        if [ "$mode" = endpoint-independent ] || [ "$mode" = shared-lan ]; then
+        if [ "$mode" = endpoint-independent ] || [ "$mode" = shared-lan ] || [ "$mode" = restricted ]; then
             ip netns exec "$namespace" nft add rule ip rustgo_netns prerouting iifname ext0 udp dport "$RG_UDP_FIRST-$RG_UDP_LAST" dnat to "$internal_ip"
+        fi
+        if [ "$mode" = endpoint-independent ] || [ "$mode" = shared-lan ]; then
             ip netns exec "$namespace" nft add rule ip rustgo_netns prerouting iifname ext0 tcp dport "$RG_TCP_FIRST-$RG_TCP_LAST" dnat to "$internal_ip"
             ip netns exec "$namespace" nft add rule ip rustgo_netns forward iifname ext0 oifname int0 udp dport "$RG_UDP_FIRST-$RG_UDP_LAST" accept
             ip netns exec "$namespace" nft add rule ip rustgo_netns forward iifname ext0 oifname int0 tcp dport "$RG_TCP_FIRST-$RG_TCP_LAST" accept
@@ -242,8 +244,10 @@ EOF
         else
             ip netns exec "$namespace" iptables -t nat -A POSTROUTING -o ext0 -j SNAT --to-source "$external_ip"
         fi
-        if [ "$mode" = endpoint-independent ] || [ "$mode" = shared-lan ]; then
+        if [ "$mode" = endpoint-independent ] || [ "$mode" = shared-lan ] || [ "$mode" = restricted ]; then
             ip netns exec "$namespace" iptables -t nat -A PREROUTING -i ext0 -p udp --dport "$RG_UDP_FIRST:$RG_UDP_LAST" -j DNAT --to-destination "$internal_ip"
+        fi
+        if [ "$mode" = endpoint-independent ] || [ "$mode" = shared-lan ]; then
             ip netns exec "$namespace" iptables -t nat -A PREROUTING -i ext0 -p tcp --dport "$RG_TCP_FIRST:$RG_TCP_LAST" -j DNAT --to-destination "$internal_ip"
             ip netns exec "$namespace" iptables -A FORWARD -i ext0 -o int0 -p udp --dport "$RG_UDP_FIRST:$RG_UDP_LAST" -j ACCEPT
             ip netns exec "$namespace" iptables -A FORWARD -i ext0 -o int0 -p tcp --dport "$RG_TCP_FIRST:$RG_TCP_LAST" -j ACCEPT
